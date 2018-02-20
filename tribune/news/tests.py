a2751b354fa52572922f943d5bac6f0887b5a453
VickyMutai <vicky.mutai96@gmail.com>
2018-02-20 12:39:15 +0300

    create models and tests for editor class

--- tribune/news/tests.py
+++ tribune/news/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
 
 # Create your tests here.
+
+class EditorTestClass(TestCase):
+    #set up method
+    def setUp(self):
+        self.vicky = Editor(first_name = 'Vicky',last_name = 'Toms',email='vicky@legacy.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.vicky,Editor))
+
+    #testing save method
+    def test_save_method(self):
+        self.vicky.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors)>0)
+
+    def test_delete_method(self):
+        self.vicky.save_editor()
+        editors=Editor.objects.all()
+        self.vicky.delete_editor()
+        self.assertTrue(len(editors)<1)
