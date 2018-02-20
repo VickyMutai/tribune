e63f0aabb033a43471dc3c00b7a719f094ba99b5
VickyMutai <vicky.mutai96@gmail.com>
2018-02-20 15:36:22 +0300

    testing methods for getting news

--- tribune/news/tests.py
+++ tribune/news/tests.py
@@ -47,6 +47,12 @@ class ArticleTestClass(TestCase):
         today_news = Article.todays_news()
         self.assertTrue(len(today_news)>0)
 
+    def test_get_news_by_date(self):
+        test_date = '2017-06-17'
+        date = dt.datetime.strptime(test_date,'%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
+
     # def test_instance(self):
     #     self.assertTrue(isinstance(self.new_article,Article))
 
