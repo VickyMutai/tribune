3cd9ee685e4ab2b2b85cf06351ad9f9d16b067b8
VickyMutai <vicky.mutai96@gmail.com>
2018-02-21 16:44:13 +0300

    deploy to heroku

--- tribune/tribune/settings.py
+++ tribune/tribune/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '44o(9zw-v-%4h&!ev6ref3angaazv0lbz%2#1zn@(=eps@#sxz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
@@ -82,9 +83,13 @@ DATABASES = {
         'NAME':'tribune',
         'USER':'vicky',
         'PASSWORD':'0716889165',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
